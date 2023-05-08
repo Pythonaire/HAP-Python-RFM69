fb9e322cafc16fe647d63220974ffe08d818bb73
Pythonaire <46537110+Pythonaire@users.noreply.github.com>
2023-05-08 08:07:31 +0200

    Update config.py

--- HAP Gateway/config.py
+++ HAP Gateway/config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """App configuration."""
 """Set configuration vars from .env file."""
-RFM69GardenURL = 'RFMGarden.local'
-RFM69HouseURL = 'RFMHouse.local'
-RFMNETWORK ={RFM69GardenURL: {"Pumpe": 11, "Weather": 12 , "RoomTwo": 14}, 
+RFM69GardenURL = 'XXXXXX.local'
+RFM69HouseURL = 'XXXXXX.local'
+RFMNETWORK ={RFM69GardenURL: {"Pump": 11, "Weather": 12 , "RoomTwo": 14}, 
             RFM69HouseURL: {"RoomOne": 13}}
 RFMMANAGE = {RFM69GardenURL: 11}
 
