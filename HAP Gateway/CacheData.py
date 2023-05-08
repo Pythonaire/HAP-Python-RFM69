a4d5a2c08a81678a3975a24c1721d7d904965ab8
Pythonaire <46537110+Pythonaire@users.noreply.github.com>
2023-05-08 08:06:42 +0200

    Update CacheData.py

--- HAP Gateway/CacheData.py
+++ HAP Gateway/CacheData.py
@@ -38,33 +38,41 @@ class RFM69Data():
                         RFM69_CACHE[node] = dict[node]
                     else:
                         RFM69_CACHE[node] = None
-            except ConnectTimeout as e:
+                logging.info('**** Cache refreshed with data from {0} ****'.format(link))
+            except ConnectionError as e:
                 logging.info('**** request to {0} timed out: {1}'.format(url, e)) 
                 for node in nodes.values():
                     RFM69_CACHE[node] = None   
-        logging.info('**** Cache refreshed with data from Nodes {0} ****'.format(list(RFM69_CACHE.keys())))
+       
 
     @classmethod
     def controlrfm(cls, node, cmd):
         global RFM69_CACHE
+        url = None
         httpsend = {node: cmd}
-        try:
-            for key, value in config.RFMMANAGE.items():
+        for key, value in config.RFMMANAGE.items():
                 if node == str(value):
                     url = 'http://' + key + ':8001/manageState'
                     break
-            RFM69_CACHE[node] = requests.get(url, json=json.dumps(httpsend), timeout= 2).json()[node]
+        if url == None:
+            logging.info('**** Node number {0} not exists in RFMMNANAGE dictionary'.format(node)) 
+        else:
+            try:
+                RFM69_CACHE[node] = requests.get(url, json=json.dumps(httpsend), timeout= 2).json()[node]
             #rfm return with json {'node':None} if request failed
-        except ConnectTimeout as e:
-            logging.info('**** request to {0} timed out: {1}'.format(url, e)) 
-            RFM69_CACHE[node] = None
-        return RFM69_CACHE[node]
+            except ConnectionError as e:
+                logging.info('**** request to {0} timed out: {1}'.format(url, e)) 
+                RFM69_CACHE[node] = None
+            return RFM69_CACHE[node]
     
+
     @classmethod
     def stateValues(cls, node, val):
         global RFM69_CACHE
         try:
             state = RFM69_CACHE[node].get(val)
+            if state==None:
+                state=0
         except Exception:
             state = 0
         return state
